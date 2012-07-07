48dcfc75cd2ee699fb0bba90a6f8b1246a8d66b4
unknown <david_000@SpeedDragon.(none)>
2012-07-07 14:03:34 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plannr::Application.config.secret_token = 'ddc363241982a1c7c8c40cf3414e6215ad37da9bdc7c04676484dc991e8694e86ae4e48400e6c1478d0f83825c93ae23b237dfaa8419136b9e0477b1f3175dba'
